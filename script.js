e6eaa22e519220a70fa433a6517f59cdcfe7db7e
araujomlopes <128427990+araujomlopes@users.noreply.github.com>
2023-03-27 22:19:19 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+<script>
+var swiper = new Swiper(".slide-content", {
+  slidesPerView: 4,
+  centeredSlides: true,
+  spaceBetween: 30,
+  grabCursor: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+</script>
